fda1ec289daee6381d0b79e7fc111854bd9c5440
igornb <igornb.mail@gmail.com>
2022-04-11 20:24:13 +0300

    logbook based feign logging with logbook-spring-boot-starter && logbook-openfeign

--- src/main/java/com/lig/feignlogging/config/FooConfiguration.java
+++ src/main/java/com/lig/feignlogging/config/FooConfiguration.java
@@ -1,12 +1,19 @@
 package com.lig.feignlogging.config;
 
+
 import feign.Logger;
-import feign.Logger.Level;
-import feign.slf4j.Slf4jLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.openfeign.FeignLogbookLogger;
 
 @Configuration
 public class FooConfiguration {
+
+	@Bean
+	public Logger feignLogger(@Autowired Logbook logbook)  {
+		return new FeignLogbookLogger(logbook);
+	}
 
 }
